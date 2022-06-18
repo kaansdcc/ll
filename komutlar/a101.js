625c0c57914e3db21909a619309a489377c2a711
Glitch (flame-feline-howler) <none>
2022-06-18 09:40:18 +0000

    🎡🐔 Checkpoint ./komutlar/a101.js:43217367/467

--- komutlar/a101.js
+++ komutlar/a101.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 exports.run = (client, message, args) => {
 let embed = new Discord.MessageEmbed()
 .setDescription('**HARCA HARCA BİTMEZ**') 
+.setImage('https://cdn.discordapp.com/attachments/987367458170101832/987651457329270805/200w.gif')
 message.channel.send(embed)
 };exports.conf = {
 enabled: true,
