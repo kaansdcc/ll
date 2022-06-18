6ed8965347d8d8b5efbb875fc3464429528f6d3e
Glitch (flame-feline-howler) <none>
2022-06-18 18:40:23 +0000

    👌😎 Checkpoint ./komutlar/davet.js:43217367/58 ./komutlar/moderasyon.js:43217367/42

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   const davet = new Discord.MessageEmbed()
   .setColor("RANDOM")
   .setTitle("Beni Sunucuna Ekle ❤️")
-  .setDescription("[__**Davet Et**__](https://discord.com/oauth2/authorize?client_id=946351219289493534&scope=bot&permissions=1342177296) \n [__**Server**__](https://discord.gg/ZF2AHFmbgN) ")
+  .setDescription("[__**Davet Et**__](https://discord.com/oauth2/authorize?client_id=946351219289493534&scope=bot&permissions=1342177296) \n [__**Server**__](https://discord.gg/WMfxcpWRZ6) ")
   message.channel.send(davet)
 }
 
