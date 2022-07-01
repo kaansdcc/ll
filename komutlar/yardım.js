529a2fd2123da3041bba339387b50bf575c4de01
Glitch (flame-feline-howler) <none>
2022-07-01 14:08:45 +0000

    💿⚽️ Checkpoint ./komutlar/yardım.js:43217367/68

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,7 +9,7 @@ const yardım = new Discord.MessageEmbed()
 .setAuthor("jarvis v12 sürümüyle sizlerle.")
 .setTitle("jarvis'in yardım menüsüne hoşgeldin! ")
  .setTimestamp()
-.setDescription("<a:dart:813712609714110504> **d+eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:gear:813714047030525982> **d+moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:sunglasses:813714441006612481> **d+kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:zap:813714783962529803> **d+logo** = __Logo komutlarını görüntülersiniz__. \n :ghost: **d+gif** = __Gif komutlarını görüntülersiniz__. ")
+.setDescription("<a:dart:813712609714110504> **d+eğlence** = __Eğlence komutlarını görüntülersiniz.__ \n <a:gear:813714047030525982> **d+moderasyon** = __Moderasyon komutlarını görüntülersiniz.__ \n <a:sunglasses:813714441006612481> **d+kullanıcı** = __Kullanıcı komutlarını görüntülersiniz.__ \n <a:zap:813714783962529803> **d+logo** = __Logo komutlarını görüntülersiniz__. \n :ghost: **d+gif** = __Gif komutlarını görüntülersiniz__. \n :loud_sound: **/sesyardım** = __Ses yardım menüsünü açar__.")
 
 .setImage("https://cdn.discordapp.com/attachments/947400801553354752/954697612995481610/game-of-thrones-white-walker.gif")
 message.channel.send(yardım)
