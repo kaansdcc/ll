d29ee942b960c6f12714095be86d7d8f62d56684
Glitch (flame-feline-howler) <none>
2022-07-06 15:14:28 +0000

    🚆👐 Checkpoint ./komutlar/yazı-tuRA:43217367/3835 ./komutlar/yazı-turu.js:43217367/24127 ./bot.js:43217367/644 ./komutlar/moderasyon.js:43217367/30

--- bot.js
+++ bot.js
@@ -55,10 +55,11 @@ client.on('message', msg => {
 });
 
 
+
 client.on("ready", () => {
   console.log(`Bütün komutlar başarıyla yüklendi!`);
   client.user.setStatus("dnd");
-  client.user.setActivity('!yardım|𝒹𝒶𝓇𝓀 𝒹𝓇𝒶𝑔𝑜𝓃','!sor bana soru sorabilirsin','Prefix:!');
+  client.user.setActivity('!yardım|𝒹𝒶𝓇𝓀 𝒹𝓇𝒶𝑔𝑜𝓃');
 })
 
 
