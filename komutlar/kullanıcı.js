62f7efb60f982e58c19437a1b0216c9a7e3e216a
Glitch (stone-impartial-sink) <none>
2022-07-13 13:49:16 +0000

    ⚽️🌯 Checkpoint ./komutlar/kullanıcı.js:41108634/101

--- komutlar/kullanıcı.js
+++ komutlar/kullanıcı.js
@@ -9,7 +9,7 @@ const kullanıcı = new Discord.MessageEmbed()
 .setAuthor("jarvis v12 sürümüyle yeniden sizlerle.")
 .setTitle(" Kullanıcı Komutları ")
  .setTimestamp()
-.setDescription(" **!avatar** = Avatarınıza bakarsınız.  \n  **!yetkilerim** = Yetkilerini görürsün.  \n  **!profil** = Profilini görürsün.  \n  **!sunucuresmi** = Sunucu resmini gösterir.  \n  **!ping** = Botun Pingine Bakarsın.  \n **!id** = Birisinin id'sine Bakarsın.  \n  **!botbilgi** = Bot istatistiklerini görürsünüz. \n **!bug-bildir** = Yazdığınız bug'u yapımcılarıma bildirir. \n **!davetlerim** = Davetlerinize bakarsınız \n **!davet-sıralaması** = Davet sıralamasına bakarsınız \n **!not** = Not alırsınız")
+.setDescription(" **!avatar** = Avatarınıza bakarsınız.  \n  **!yetkilerim** = Yetkilerini görürsün.    \n  **!sunucuresmi** = Sunucu resmini gösterir.  \n  **!ping** = Botun Pingine Bakarsın.  \n **!id** = Birisinin id'sine Bakarsın.  \n  **!botbilgi** = Bot istatistiklerini görürsünüz. \n **!bug-bildir** = Yazdığınız bug'u yapımcılarıma bildirir. \n **!davetlerim** = Davetlerinize bakarsınız \n **!davet-sıralaması** = Davet sıralamasına bakarsınız \n **!not** = Not alırsınız \n **!randompp** = Botun olduğu serverlardaki ppleri görürsünüz")
 .setImage("https://cdn.discordapp.com/attachments/947400801553354752/954697612995481610/game-of-thrones-white-walker.gif")
 message.channel.send(kullanıcı)
 }
