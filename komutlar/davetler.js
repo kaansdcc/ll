002edbeedf4dd70321d20c99fc6d803d39e09656
Glitch (wiggly-reminiscent-stranger) <none>
2022-07-18 07:55:42 +0000

    🚖✂️ Checkpoint ./komutlar/davetler.js:41138121/1726 ./Helpers/Database.js:41138121/2

--- komutlar/davetler.js
+++ komutlar/davetler.js
@@ -1,25 +1,27 @@
 const Discord = require("discord.js");
-const ayarlar = require("../ayarlar.json")
-
-exports.run = async (client, message, args, themiss) => {
-
-   message.guild.fetchInvites().then(invs => {
-      let user = message.mentions.users.first() || message.author
-      let personalInvites = invs.filter(i => i.inviter.id === user.id);
-      let davetsayi = personalInvites.reduce((p, v) => v.uses + p, 0);
-return message.channel.send(`${user} - Davetinizle gelen kişi sayısı: ${davetsayi}`);
-   })
-}
-
+const Database = require("../Helpers/Database");
+// exports.onLoad = (client) => {};
+/**
+ * @param {Discord.Client} client 
+ * @param {Discord.Message} message 
+ * @param {Array<String>} args 
+ */
+exports.run = async (client, message, args) => {
+    const db = new Database("./Servers/" + message.guild.id, "Invites");
+    var data = db.get(`invites.${message.member.id}`) || { total: 0, fake: 0, inviter: null, regular: 0, bonus: 0, leave: 0 };
+    var embed = new Discord.MessageEmbed()
+    .setDescription(`**Toplam:** \`${(data.total || 0) + (data.bonus || 0)}\`, **Düzenli** \`${data.regular || 0}\`, **Bonus:** \`${data.bonus || 0}\`, **Çıkanlar:** \`${data.leave || 0}\`, (**Sahte Olanlar:** \`${data.fake || 0}\`)`)
+    .setColor("RANDOM");
+    message.channel.send(embed);
+};
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: ["davetsayım"],
+  aliases: [],
   permLevel: 0
 };
-
 exports.help = {
   name: 'davetlerim',
-  description: 'Davetinizle gelen kişi sayısını gösterir',
-  usage: 'davetlerim'
-}
+  description: 'Logo Yaparsınız',
+  usage: 'm-logo <yazı>'
+};
